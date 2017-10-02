3b4e94b35ae0c8bd292d60dfc010b8abc3748630
Jonathan Sax <jsax8310@outlook.com>
2017-10-02 08:31:18 -0600

    touch up

--- components/KegStatus.js
+++ components/KegStatus.js
@@ -26,7 +26,7 @@ export default class KegStatus extends Component {
         <Card style={{flex: 1}}>
           <CardItem>
             <Left>
-              <Thumbnail source={{uri: 'http://blog.patternbank.com/wp-content/uploads/2012/12/vintage-beer-labels-michigan-all-over-beer-02-700x733.jpg'}} />
+              <Thumbnail source={{uri: 'https://slack-imgs.com/?c=1&url=https%3A%2F%2Fstatic1.squarespace.com%2Fstatic%2F5519a5eae4b0d37c5805566b%2F553ea190e4b0e1b549d3fd21%2F5565fe80e4b02cc99128c794%2F1432747650173%2FMojo6Pack_WEB.jpg%3Fformat%3D750w'}} />
               <Body>
                 <Text>Mojo IPA</Text>
                 <Text note>ABV: 12%</Text>
